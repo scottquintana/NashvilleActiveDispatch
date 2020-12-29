7fa7985035ffccf43ea85340a10d24060516bc3e
Scott Quintana <mail@scottquintana.com>
2020-12-29 15:05:52 -0600

    Initial Commit

---
+++ NashvillePoliceAlerts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NashvillePoliceAlerts
+//
+//  Created by Scott Quintana on 12/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
