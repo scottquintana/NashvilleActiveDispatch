23132a74564842685f2cf674b5d656f335df0e10
Scott Quintana <60410048+scottquintana@users.noreply.github.com>
2026-05-27 18:25:54 -0700

    hide keys

--- NashvilleActiveDispatch/AppDelegate.swift
+++ NashvilleActiveDispatch/AppDelegate.swift
@@ -9,25 +9,13 @@ import FirebaseCore
 import PostHog
 import UIKit
 
-enum PostHogEnv: String {
-    case projectToken = "POSTHOG_PROJECT_TOKEN"
-    case host = "POSTHOG_HOST"
-
-    var value: String {
-        guard let value = ProcessInfo.processInfo.environment[rawValue] else {
-            fatalError("Set \(rawValue) in the Xcode scheme environment variables.")
-        }
-        return value
-    }
-}
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
 
-        let config = PostHogConfig(apiKey: PostHogEnv.projectToken.value, host: PostHogEnv.host.value)
+        let config = PostHogConfig(apiKey: Secrets.postHogProjectToken, host: Secrets.postHogHost)
         config.captureApplicationLifecycleEvents = true
         PostHogSDK.shared.setup(config)
 
@@ -37,17 +25,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
